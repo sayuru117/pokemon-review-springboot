c62380623c639bae10bdc718fa635f50d3850a87
sayuru117 <sayuru117@gmail.com>
2024-07-26 22:55:09 +0530

    commit 26072024

--- src/test/java/com/pokemonreview/api/service/PokemonServiceTest.java
+++ src/test/java/com/pokemonreview/api/service/PokemonServiceTest.java
@@ -11,12 +11,18 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -24,6 +30,11 @@ public class PokemonServiceTest {
 
 	@Mock
 	private PokemonRepository pokemonRepository;
+
+	@Mock
+	PokemonServiceImpl pokemonService1;
+
+
 
 	@InjectMocks
 	private PokemonServiceImpl pokemonService;
@@ -57,6 +68,47 @@ public class PokemonServiceTest {
 		 assertThat(pokemonResponse).isNotNull();
 
 	 }
+
+	@Test
+	public void whenNotUseMockAnnotation_thenCorrect() {
+		List mockList = Mockito.mock(ArrayList.class);
+
+		mockList.add("one");
+		Mockito.verify(mockList).add("one");
+		assertEquals(0, mockList.size());
+
+		Mockito.when(mockList.size()).thenReturn(100);
+		assertEquals(100, mockList.size());
+	}
+
+	@Test
+	public void myMockMethod(){
+		spiedList.add("one");
+		verify(spiedList).add("one");
+		assertEquals(0, spiedList.size());
+
+		Mockito.when(spiedList.size()).thenReturn(100);
+		assertEquals(100, spiedList.size());
+
+
+	}
+	@Spy
+	List<String> spiedList = new ArrayList<>();
+
+	@Test
+	public void whenUseSpyAnnotation_thenSpyIsInjectedCorrectly() {
+		spiedList.add("one");
+		spiedList.add("two");
+
+		Mockito.verify(spiedList).add("one");
+		Mockito.verify(spiedList).add("two");
+
+		assertEquals(2, spiedList.size());
+
+		Mockito.doReturn(100).when(spiedList).size();
+		assertEquals(100, spiedList.size());
+	}
+
 
 
 }
